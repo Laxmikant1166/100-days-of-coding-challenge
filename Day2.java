79d660890950bf8137dd3be33e5d30bcec55b151
Laxmikant Ghodake <151601776+Laxmikant1166@users.noreply.github.com>
2024-09-24 18:14:57 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,15 @@
+public class Day2{
+    public static void main(String args[]){
+        int age;
+        age = 20;
+        Double percentage;
+        percentage = 89.40;
+        String name;
+        name = "Aniket";
+
+        System.out.println("Age:"+age);
+        System.out.println("Percentage:"+percentage);
+        System.out.println("Name:"+name);
+
+    }
+}
