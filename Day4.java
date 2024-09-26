aaf8cf38b3d9b0f5fa89b9c333837d12e2b08f45
Laxmikant Ghodake <151601776+Laxmikant1166@users.noreply.github.com>
2024-09-26 23:20:45 +0530

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Day4 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the value of a:");   
+     int a = sc.nextInt();
+     System.out.println("Enter the value of b:");
+     int b = sc.nextInt();
+     int c = a + b;
+     System.out.println("Sum="+c);
+     System.out.println("Enter the height of ractangle:");
+     double height = sc.nextDouble();
+     System.out.println("Enter the width of ractangle:");
+     double width = sc.nextDouble();
+     double area = height * width;
+     System.out.println("Area of rectangle="+area);
+     
+
+
+
+
+
+    }
+    
+}
