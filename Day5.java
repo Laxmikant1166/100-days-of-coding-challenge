42101609bcbf1e5ff966e93bf01e79d9cbe51e0c
Laxmikant Ghodake <151601776+Laxmikant1166@users.noreply.github.com>
2024-09-30 23:39:58 +0530

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,32 @@
+public class Day5 {
+    public static void main(String[] args) {
+        
+    
+    int a = 5;
+    int b = 10;
+    System.out.println("postincreament of a ="+ --a);
+    System.out.println("predecreament of a="+ ++a);
+
+    System.out.println("postincreament of b ="+ ++b);
+    System.out.println("predecreament of b="+--b);
+
+     b+=a;
+     System.out.println("b+=a:"+b);    
+
+     b-=a;
+     System.out.println("b-=a:"+b); 
+     
+     b*=a;
+     System.out.println("b*=a:"+b);    
+
+     b/=a;
+     System.out.println("b/=a:"+b);   
+     
+     b%=a;
+     System.out.println("b%=a"+b);
+
+
+
+    
+}
+}
